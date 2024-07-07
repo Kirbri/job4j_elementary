719baa547ed4b818e914ce64c95c54edb56c0e08
AnnKornilova <kanna1997@mail.ru>
2024-07-07 15:20:13 +0300

    6.6.0. Двумерный массив [#226796 #136479]

---
+++ src/main/java/ru/job4j/array/Sequence.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+public class Sequence {
+    public static void main(String[] args) {
+        int[][] array = new int[3][3];
+        array[0][0] = 1;
+        array[0][1] = 2;
+        array[0][2] = 3;
+        array[1][0] = 4;
+        array[1][1] = 5;
+        array[1][2] = 6;
+        array[2][0] = 7;
+        array[2][1] = 8;
+        array[2][2] = 9;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                System.out.println(array[i][j]);
+            }
+        }
+
+        for (int[] arr1 : array) {
+            for (int arr2 : arr1) {
+                System.out.println(arr2);
+            }
+        }
+    }
+}
