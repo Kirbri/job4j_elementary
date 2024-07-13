9ee915ec5a9701bd89433acb0f472eb4e6d3e4fc
AnnKornilova <kanna1997@mail.ru>
2024-07-13 17:59:17 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587 #136618]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -9,45 +9,37 @@ class PointTest {
     @Test
     void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double output = Point.distance(x1, y1, x2, y2);
+        Point x = new Point(0, 2);
+        Point y = new Point(0, 0);
+        double output = x.distance(y);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when19to08then1dot4142() {
         double expected = 1.4142;
-        int x1 = 1;
-        int x2 = 0;
-        int y1 = 9;
-        int y2 = 8;
+        Point x = new Point(1, 9);
+        Point y = new Point(0, 8);
         double value = 0.0001f;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = x.distance(y);
         assertThat(output).isEqualTo(expected, withPrecision(value));
     }
 
     @Test
     void when6Minus9to7Minus3then6dot08() {
         double expected = 6.08;
-        int x1 = 6;
-        int y1 = -9;
-        int x2 = 7;
-        int y2 = -3;
-        double output = Point.distance(x1, y1, x2, y2);
+        Point x = new Point(6, -9);
+        Point y = new Point(7, -3);
+        double output = x.distance(y);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void whenMinus600to0Minus100then116dot619() {
         double expected = 116.619;
-        int x1 = -60;
-        int y1 = 0;
-        int x2 = 0;
-        int y2 = -100;
-        double output = Point.distance(x1, y1, x2, y2);
+        Point x = new Point(-60, 0);
+        Point y = new Point(0, -100);
+        double output = x.distance(y);
         assertThat(output).isEqualTo(expected, withPrecision(0.001));
     }
 }
